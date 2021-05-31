31092da10713bce9ff8647d11cc411e4a0e0c896
akapur99 <abhi.kapur09@gmail.com>
2021-05-31 17:01:26 -0400

    FULLY WORKING

--- app.py
+++ app.py
@@ -148,47 +148,103 @@ def get_response(analyzer, text):
 
 def program(api, username, silent=False, mode='hour',
             count=50, too_many_tweets=1000, max_attempts=5):
-    app.logger.info('HERE')
-    endDate = datetime.datetime.now()
+    
+    data_path = './results/{}.csv'.format(username)
+    if not os.path.isfile(data_path):
+        data = pd.DataFrame(columns=['id', 'created_at', 'full_text', 'positivity',
+                                     'sadness', 'joy', 'fear', 'disgust', 'anger', 
+                                     'categories', 'concepts', 'entities'])    
+        data.to_csv(data_path, sep=',', index=False) 
+    data = pd.read_csv(data_path, sep=',')
+    data['created_at'] = pd.to_datetime(data['created_at'], format='%Y-%m-%d %H:%M:%S')
     if mode == 'hour':
+        endDate = datetime.datetime.now()
         timedelta = datetime.timedelta(hours=1)
-    elif mode =='week':
-        timedelta = datetime.timedelta(weeks=1)
-    startDate = endDate - timedelta
-    tweets = get_tweets_for_user_within_dates(api, username, 
-                                              startDate, endDate,
-                                              count, too_many_tweets,
-                                              max_attempts)
-    startDate_endDate_str = startDate.strftime("%Y-%m-%d_%H:%M") \
-                            + '_' + endDate.strftime("%Y-%m-%d_%H:%M")
-    if not silent:
+        startDate = endDate - timedelta
+        tweets = get_tweets_for_user_within_dates(api, username, 
+                                                  startDate, endDate,
+                                                  count, too_many_tweets,
+                                                  max_attempts)
+        results = {'sentiment': [], 'emotion': []}
         n = len(tweets)
-        print('from: {}, to: {}, number of tweets: {}'.format(startDate,
-                                                              endDate, 
-                                                              n))
-        if n > 0:
-            print(utc_to_local(tweets[0].created_at), utc_to_local(tweets[-1].created_at))
-    id_lst, created_at_lst, full_text_lst, response_lst = [], [], [], []
-    for tweet in tqdm(tweets, disable=silent):
-        id_lst.append(tweet.id)
-        created_at_lst.append(utc_to_local(tweet.created_at))
-        full_text_lst.append(tweet.full_text.replace('\n', ' '))
-        response_lst.append(get_response(my_analyzer, tweet.full_text))
-    data = pd.DataFrame({
-        'id': id_lst,
-        'created_at': created_at_lst, 
-        'full_text': full_text_lst,
-        'response': response_lst,
-    })
-    data.to_csv('./results/user.csv', index=False)
-    print(data, flush=True)
-    return data
+        if n == 0:
+            print('no tweets found')
+            return results
+        if not silent:
+            print('from: {}, to: {}, number of tweets: {}'.format(startDate,
+                                                                  endDate, 
+                                                                  n))
+        data_ids = data['id']
+        positivity_lst = []
+        sadness_lst, joy_lst, fear_lst, disgust_lst, anger_lst = [], [], [], [], []
+        found_new_tweet = False
+        for tweet in tqdm(tweets, disable=silent):
+            if tweet.id in data_ids:
+                continue
+            found_new_tweet = True
+            response = get_response(my_analyzer, tweet.full_text)
+            sentiment = response["sentiment"]["document"]
+            row = {"id": tweet.id, "created_at": utc_to_local(tweet.created_at),
+                   "full_text": tweet.full_text.replace('\n', ' '),
+                   "positivity": 0.5 if sentiment['label'] == 'neutral' else (sentiment['score'] + 1) / 2,
+                   "sadness": 0, "joy": 0, "fear": 0, "disgust": 0, "anger": 0,
+                   "categories": response["categories"], 
+                   "concepts": response["concepts"],
+                   "entities": response["entities"]}
+            for emotion, confidence in response['emotion']['document']['emotion'].items():
+                row[emotion] = confidence
+            data = data.append(row, ignore_index=True)
+            positivity_lst.append(row['positivity'])
+            sadness_lst.append(row['sadness']); joy_lst.append(row['joy']); fear_lst.append(row['fear'])
+            disgust_lst.append(row['disgust']); anger_lst.append(row['anger'])
+        if not found_new_tweet:
+            print('no new tweets found')
+            return results
+        data.to_csv(data_path, sep=',', index=False)
+        positivity_avg = np.mean(positivity_lst)
+        sadness_avg = np.mean(sadness_lst); joy_avg = np.mean(joy_lst); fear_avg = np.mean(fear_lst)
+        disgust_avg = np.mean(disgust_lst); anger_avg = np.mean(anger_lst)
+        for sentiment, (interval_low, interval_high) in sentiment_label_2_bin.items():
+            if interval_low <= positivity_avg < interval_high:
+                results['sentiment'].append(sentiment)
+                break
+            elif interval_high == 1 and positivity_avg == 1:
+                results['sentiment'].append(sentiment)
+                break
+        for emotion, (interval_low, interval_high) in emotion_label_2_bin.items():
+            emotion_avg = locals()[emotion+'_avg']
+            if interval_low <= emotion_avg < interval_high:
+                results['emotion'].append(emotion)
+            elif interval_high == 1 and emotion_avg == 1:
+                results['emotion'].append(emotion)
+        print('tweets analyzed')
+        return results
+    elif mode == 'week':
+        data_avg_path = './results/{}_avg.csv'.format(username)
+        if not os.path.isfile(data_avg_path):
+            data_avg = pd.DataFrame(columns=['start_date', 'end_date', 'positivity_avg',
+                                             'sadness_avg', 'joy_avg', 'fear_avg', 
+                                             'disgust_avg', 'anger_avg'])    
+            data_avg.to_csv(data_avg_path, sep=',', index=False) 
+        data_avg = pd.read_csv(data_avg_path, sep=',')
+        endDate = round_seconds(datetime.datetime.now())
+        timedelta = datetime.timedelta(weeks=1)
+        startDate = endDate - timedelta
+        data_week = data[data['created_at'].between(startDate, endDate)]
+        data_week_avg = data_week.mean(axis=0)
+        row = {'start_date': startDate, 'end_date': endDate,
+            'positivity_avg': data_week_avg['positivity'],
+            'sadness_avg': data_week_avg['sadness'],
+            'joy_avg': data_week_avg['joy'],
+            'fear_avg': data_week_avg['fear'],
+            'disgust_avg': data_week_avg['disgust'],
+            'anger_avg': data_week_avg['anger']}
+        data_avg = data_avg.append(row, ignore_index=True)
+        data_avg = data_avg.round(2)
+        data_avg.to_csv(data_avg_path, sep=',', index=False)
 
 
-
-
-
-def send_message(from_="+14193860121", to="+17814285958"):
+def send_message(emotion=None, from_="+14193860121", to="+17814285958"):
 
     
     rndm = random.randint(0, 101)
@@ -196,8 +252,13 @@ def send_message(from_="+14193860121", to="+17814285958"):
     rndm2 = random.randint(0, 239)
     URLstr = tracks['URL'][rndm2] + ' \n'
 
-    msg = 'Hey there, you seem to be feeling a little down, just wanted to check in and drop a meditation link if that helps <3 ' + URLstr + quotestr
-
+    msg = ''
+    if not emotion:
+        msg = 'Hey there, you seem to be feeling a little down, just wanted to check in and drop a meditation link if that helps <3 ' + URLstr + quotestr
+    else:
+        emotionstr = ' and '.join(emotion)
+        msg = 'Hey there, you seem to be feeling a little ' + emotionstr + ', just wanted to check in and drop a meditation link if that helps <3 ' + URLstr + quotestr
+        
 
     message = client.messages \
                 .create(
@@ -206,7 +267,7 @@ def send_message(from_="+14193860121", to="+17814285958"):
                      to=to
                  )
     print(msg, flush=True)
-    
+
 
 # INIT
 auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
@@ -223,19 +284,36 @@ my_analyzer.set_service_url(WATSON_URL)
 quotes = pd.read_json('quotes.json').quotes
 tracks = pd.read_csv('links.csv')
 
+# Apply thresholds for sentiment
+sentiment_label_2_bin = {'negative': (0, 0.43), 'neutral': (0.43, 0.7), 'positive': (0.7, 1)}
+# Apply thresholds for emotion
+emotion_label_2_bin = {'joy': (0.58, 1), 
+                    'sadness': (0.55, 1), 
+                    'fear': (0.55, 1), 
+                    'disgust': (0.55, 1), 
+                    'anger': (0.55, 1)}
+
 
 
 @app.route("/")
 def hello_world():
     
     print('Hola', flush=True)
-    # mode = 'hour'
-    # username = 'elonmusk'
-    # count = 50
-    # too_many_tweets = 1000
-    # max_attempts = 10
-    # silent = False
-    # d = program(api, username, silent, mode, count, too_many_tweets, max_attempts)
+    mode = 'hour'
+    username = 'MiDJs4U'
+    count = 50
+    too_many_tweets = 1000
+    max_attempts = 10
+    silent = False
+    d = program(api, username, silent, mode, count, too_many_tweets, max_attempts)
+    print(d, flush=True)
+
+    if not d or (d['sentiment'][0] == 'neutral' or d['sentiment'][0] == 'positive'):
+        return 'done with no message'
+
+    send_message(d['emotion'])
+    
+    
 
     # scheduler = BlockingScheduler()
     # _ = scheduler.add_job(lambda: program(api, username, silent, mode,
@@ -243,14 +321,8 @@ def hello_world():
     #                                   max_attempts), 
     #                  'interval', minutes=2)
     # scheduler.start()
-
-    # boo = True
-    send_message()
-
-    # if boo:
-    #     send_message()
         
-    return 'done'
+    return 'done with message'
 
 # @app.route("/get_tweet")
 # def hello_world():
@@ -258,10 +330,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-# Get tweets every hour
-# Run sentiment analysis
-# If SOME_METRIC < THRESHOLD:
-# TRIGGER MESSAGE
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import json
 import pandas as pd
 
-df = pd.read_csv('meditation_playlist.csv')
-df['SpotifyID'] = df['SpotifyID'].map(lambda x: 'http://open.spotify.com/track/' + x.strip())
-df.rename(columns={"SpotifyID":"URL"}, inplace=True)
+# df = pd.read_csv('meditation_playlist.csv')
+# df['SpotifyID'] = df['SpotifyID'].map(lambda x: 'http://open.spotify.com/track/' + x.strip())
+# df.rename(columns={"SpotifyID":"URL"}, inplace=True)
 
-df.to_csv('links.csv')
+# df.to_csv('links.csv')
 
-print(df)
+# print(df)
+
+emotions = ['sad', 'bad', 'cat']
+
+emotion = ' and '.join(emotions)
+print(emotion)
